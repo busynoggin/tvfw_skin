e79d5c2b9f11f21e2c9142d1ff2f9ed0fc5422c5
Jeff Segars <jeff@busynoggin.com>
2016-02-19 09:03:13 -0600

    Initial add of code from jgreenawalt

---
+++ ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "BusyNoggin.TvfwSkin".
+ *
+ * Auto generated 04-02-2016 21:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Busy Noggin TVFW Base Skin',
+	'description' => 'Base skin for transition from TemplaVoila Framework to Fluid',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'cms,extbase,fluid,flux,fluidpages,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Jeremy Greenawalt',
+	'author_email' => 'jeremy.greenawalt@busynoggin.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-7.6.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '',
+
+			'fluidpages' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
